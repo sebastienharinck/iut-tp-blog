88fd8a18c6f7cc6a6973ef8f945aa8cc7ad074a1
Noofly <boiboigire59@gmail.com>
2019-10-16 11:03:30 +0200

    Simplified credentials configuration

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,13 @@
 <?php
 
 //database credentials
-define('DB_HOST', 'db');
-define('DB_NAME', 'postgres');
-define('DB_PORT', '5432');
-define('DB_USER', 'postgres');
-define('DB_PASS', 'password');
+define('DB_HOST', 'db'); //à modifier si vous utilisez "localhost"
+define('DB_NAME', 'dm_name'); //à modifier
+//define('DB_PORT', '5432'); //->Pas nécessaire
+define('DB_USER', 'root');
+define('DB_PASS', '');//root sur Linux/MAC, rien sur Windows
 
-$db = new PDO('pgsql:dbname='.DB_NAME.';host='.DB_HOST, DB_USER, DB_PASS);
+$db = new PDO('mysql:dbname='.DB_NAME.';host='.DB_HOST, DB_USER, DB_PASS);//Modifier "mysql" si vous utilisez un autre SGBD
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 require_once 'functions.php';
